ceeb6f6ed00c0e26f69edd683895f0f616c55918
skyclouds2001 <skyclouds2001@163.com>
2022-10-04 20:50:42 +0800

    feat: add legacy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react(), eslint(), legacy()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/')
