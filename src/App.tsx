afbca7186ab41ff01cbe6e60a3835ec36adedcde
skyclouds2001 <skyclouds2001@163.com>
2022-10-04 21:31:01 +0800

    feat: change route position

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/es/locale/zh_CN'
 import routes from './router'
@@ -8,13 +8,11 @@ import './App.css'
 export default function App(): JSX.Element {
   return (
     <ConfigProvider locale={zhCN}>
-      <Router>
-        <Routes>
-          {routes.map(v => {
-            return <Route key={v.path} path={v.path} element={v.component} />
-          })}
-        </Routes>
-      </Router>
+      <Routes>
+        {routes.map(v => {
+          return <Route key={v.path} path={v.path} element={v.component} />
+        })}
+      </Routes>
     </ConfigProvider>
   )
 }
