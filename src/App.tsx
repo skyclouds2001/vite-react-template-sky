5d13908499f3e3deccfac5816b980c113dcf17bd
skyclouds2001 <skyclouds2001@163.com>
2022-10-04 21:15:07 +0800

    feat: add ant-design language config

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/es/locale/zh_CN'
 import routes from './router'
 import './App.css'
 
 export default function App(): JSX.Element {
   return (
-    <Router>
-      <Routes>
-        {routes.map(v => {
-          return <Route key={v.path} path={v.path} element={v.component} />
-        })}
-      </Routes>
-    </Router>
+    <ConfigProvider locale={zhCN}>
+      <Router>
+        <Routes>
+          {routes.map(v => {
+            return <Route key={v.path} path={v.path} element={v.component} />
+          })}
+        </Routes>
+      </Router>
+    </ConfigProvider>
   )
 }
