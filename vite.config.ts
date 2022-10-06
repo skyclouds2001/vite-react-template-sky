0c71d5c121529753d27f9a1c4fa78f6d8502d1b4
skyclouds2001 <skyclouds2001@163.com>
2022-10-06 23:57:19 +0800

    feat: add stylelint in vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 import eslint from 'vite-plugin-eslint'
+import stylelint from 'vite-plugin-stylelint'
 
 export default defineConfig({
-  plugins: [react(), eslint(), legacy()],
+  plugins: [react(), eslint(), legacy(), stylelint()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/')
