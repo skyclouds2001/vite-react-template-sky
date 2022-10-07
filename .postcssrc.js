706a8572d3e1a6293c5b1536dc43babad39c1143
skyclouds2001 <skyclouds2001@163.com>
2022-10-07 10:50:36 +0800

    feat: add tailwindcss

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,6 +1,7 @@
 const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
+const tailwindcss = require('tailwindcss')
 
 module.exports = {
-  plugins: [autoprefixer, cssnano]
+  plugins: [autoprefixer, cssnano, tailwindcss]
 }
