08fa562e0a86f4960bf6b2b5196de7ed6167631e
skyclouds2001 <skyclouds2001@163.com>
2022-10-07 13:13:07 +0800

    feat: add icons

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setCounter } from '@/store/modules/main'
 import { RootStore } from '@/store'
+import { PlusOutlined } from '@ant-design/icons'
 
 export default function Main(): JSX.Element {
   const dispatch = useDispatch()
@@ -17,6 +18,7 @@ export default function Main(): JSX.Element {
   return (
     <div>
       <span>Main</span>
+      <PlusOutlined />
       <div>
         <input value={value} onChange={e => setValue(parseInt(e.target.value))} />
         <button onClick={() => dispatch(setCounter({ counter: value }))}>保存</button>
