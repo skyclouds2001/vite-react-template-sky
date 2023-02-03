7d0772834cdbfdeb805887168d2adcd3445ca788
skyclouds2001 <skyclouds2001@163.com>
2023-02-03 20:09:34 +0800

    build: update lint-staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,jsx,tsx,ts}': ['npx eslint --fix'],
-  '*.{js,jsx,ts,tsx,md,html,css,sass,scss,less,styl,json}': ['npx prettier --write .'],
-  '*.{css,sass,scss,less,styl}': ['npx stylelint'],
+  '*.{js,jsx,ts,tsx,json,vue,html,md,mdx}': ['eslint --fix'],
+  '*.{js,jsx,ts,tsx,json,vue,html,md,mdx,yml,yaml,css,sass,scss,less,styl,stylus,sss}': ['prettier --write'],
+  '*.{js,jsx,ts,tsx,vue,html,md,mdx,css,sass,scss,less,styl,stylus,sss}': ['stylelint --fix'],
 }
