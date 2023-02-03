a7b0058334f0c441703363614dcafe8612d2fa92
skyclouds2001 <skyclouds2001@163.com>
2023-02-03 20:46:38 +0800

    build: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,15 @@ module.exports = {
     react: {
       version: 'detect',
     },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: 'tsconfig.json',
+      },
+    },
   },
   overrides: [
     {
