cfd25dfd3e1c1f9e52198e99a375c06acdc53519
skyclouds2001 <skyclouds2001@163.com>
2023-02-04 22:26:58 +0800

    build: rename folders under src folder

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,7 @@
 import React, { lazy } from 'react'
 import { useRoutes } from 'react-router-dom'
 
-const Main = lazy(() => import('@/view/main'))
+const Main = lazy(() => import('@/views/main'))
 
 const Router: React.FC = () => {
   return useRoutes([
