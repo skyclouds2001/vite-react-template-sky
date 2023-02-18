633cc0ce4c00c81438bb5ecae0777643c0028421
skyclouds2001 <skyclouds2001@163.com>
2023-02-19 01:28:05 +0800

    build: remove stylelint with prettier

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-recess-order', 'stylelint-prettier/recommended'],
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   plugins: ['stylelint-order'],
   rules: {
     'at-rule-no-unknown': [
