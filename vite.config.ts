a06a8604f79947e00c6c477ba30a0a939f844cd2
skyclouds2001 <skyclouds2001@163.com>
2023-02-24 22:05:58 +0800

    build: update vite config for vite eslint plugin and vite stylelint plugin cache

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,18 @@ import eslint from 'vite-plugin-eslint'
 import stylelint from 'vite-plugin-stylelint'
 
 export default defineConfig({
-  plugins: [react(), legacy(), eslint(), stylelint()],
+  plugins: [
+    react(),
+    legacy(),
+    eslint({
+      cache: true,
+      cacheLocation: 'node_modules/.vite/.eslintcache',
+    }),
+    stylelint({
+      cache: true,
+      cacheLocation: 'node_modules/.vite/.stylelintcache',
+    }),
+  ],
   css: {
     postcss: 'postcss.config.js',
     devSourcemap: true,
