e6344d81d181260cc068adbfdbd8f64858bf079e
skyclouds2001 <skyclouds2001@163.com>
2023-02-25 13:24:18 +0800

    build: add rollup-plugin-visualizer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 import eslint from 'vite-plugin-eslint'
 import stylelint from 'vite-plugin-stylelint'
+import visualizer from 'rollup-plugin-visualizer'
 
 export default defineConfig({
   plugins: [
@@ -16,6 +17,13 @@ export default defineConfig({
     stylelint({
       cache: true,
       cacheLocation: 'node_modules/.vite/.stylelintcache',
+    }),
+    visualizer({
+      filename: 'report.html',
+      title: 'report',
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
     }),
   ],
   css: {
