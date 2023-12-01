12b998c843551e1ab25dee5f2f840d7328ab1be2
skyclouds2001 <skyclouds2001@163.com>
2023-12-01 19:54:07 +0800

    build: add pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'node:path'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 import { viteMockServe as mock } from 'vite-plugin-mock'
+import { VitePWA as pwa } from 'vite-plugin-pwa'
 import visualizer from 'rollup-plugin-visualizer'
 import checker from 'vite-plugin-checker'
 
@@ -11,6 +12,25 @@ export default defineConfig({
     react(),
     legacy(),
     mock(),
+    pwa({
+      registerType: 'autoUpdate',
+      manifest: {
+        start_url: '/',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        lang: 'zh-CN',
+      },
+    }),
     checker({
       typescript: true,
       eslint: {
