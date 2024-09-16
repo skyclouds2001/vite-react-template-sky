14a0c1c92515759e8011ec7b17e57e27645df2ac
skyclouds2001 <skyclouds2001@163.com>
2024-09-16 21:41:03 +0800

    build: update manifest

--- vite.config.ts
+++ vite.config.ts
@@ -12,22 +12,7 @@ export default defineConfig({
     legacy(),
     pwa({
       registerType: 'autoUpdate',
-      manifest: {
-        start_url: '/',
-        icons: [
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-        lang: 'zh-CN',
-      },
+      manifest: false,
     }),
     checker({
       typescript: true,
