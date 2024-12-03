5a682114601e65cff2ce016ffd303d89831a1ec9
skyclouds2001 <skyclouds2001@163.com>
2024-12-04 00:58:28 +0800

    build: update json stringify config

--- vite.config.ts
+++ vite.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
   },
   json: {
     namedExports: false,
-    stringify: true,
+    stringify: 'auto',
   },
   server: {
     host: env.VITE_DEV_SERVER_HOST ?? 'localhost',
